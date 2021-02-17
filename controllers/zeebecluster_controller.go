1f8e2fa49f8737abf6e7b69dd036e6ce8cd7ccbe
salaboy <salaboy@gmail.com>
2021-02-17 16:27:14 -0300

    initial commit

---
+++ controllers/zeebecluster_controller.go
@@ -0,0 +1,189 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"github.com/go-logr/logr"
+	cc "github.com/salaboy/camunda-cloud-go-client/pkg/cc/client"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"log"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"time"
+
+	zeebev1 "zeebe.io/m/v2/api/v1"
+)
+
+const (
+	interval = 10 * time.Second
+)
+
+func (r *ZeebeClusterReconciler) WaitForClusterStateChange(clusterId string, currentStatus cc.ClusterStatus) (cc.ClusterStatus, error) {
+
+	wait.PollImmediateUntil(interval, func() (bool, error) {
+
+		resp, err := cc.GetClusterDetails(clusterId)
+
+		if err != nil {
+
+			return false, err
+		}
+
+		log := r.Log.WithValues("ClusterId: ", clusterId, "CurrentState: ", currentStatus)
+		log.Info("Reported by Camunda Cloud: ", "Cluster State: ", resp.Ready)
+
+		if resp.Ready == currentStatus.Ready {
+			return false, nil
+		}
+		return true, nil
+
+	}, nil)
+	return cc.GetClusterDetails(clusterId)
+
+}
+
+// ZeebeClusterReconciler reconciles a ZeebeCluster object
+type ZeebeClusterReconciler struct {
+	client.Client
+	Log logr.Logger
+}
+
+func ignoreNotFound(err error) error {
+	if errors.IsNotFound(err) {
+		return nil
+	}
+	return err
+}
+
+// +kubebuilder:rbac:groups=zeebe.io.zeebe,resources=zeebeclusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=zeebe.io.zeebe,resources=zeebeclusters/status,verbs=get;update;patch
+
+func (r *ZeebeClusterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("zeebecluster > ", req.NamespacedName)
+
+	var zeebeCluster zeebev1.ZeebeCluster
+
+	log.Info(">>> Fetching Info about resources: " + req.NamespacedName.Name)
+
+	err := r.Get(ctx, req.NamespacedName, &zeebeCluster)
+	if err != nil {
+		// handle error
+		log.Error(err, "Failed to get Zeebe Cluster")
+	}
+	// name of your custom finalizer
+	myFinalizerName := "zeebecluster.cloud.camunda.com"
+
+	if zeebeCluster.ObjectMeta.DeletionTimestamp.IsZero() {
+		// The object is not being deleted, so if it does not have our finalizer,
+		// then lets add the finalizer and update the object.
+		if !containsString(zeebeCluster.ObjectMeta.Finalizers, myFinalizerName) {
+			zeebeCluster.ObjectMeta.Finalizers = append(zeebeCluster.ObjectMeta.Finalizers, myFinalizerName)
+			if err := r.Update(context.Background(), &zeebeCluster); err != nil {
+				return reconcile.Result{}, err
+			}
+		}
+	} else {
+		// The object is being deleted
+		if containsString(zeebeCluster.ObjectMeta.Finalizers, myFinalizerName) {
+			// our finalizer is present, so lets handle our external dependency
+			if err := r.deleteExternalDependency(&zeebeCluster); err != nil {
+				// if fail to delete the external dependency here, return with error
+				// so that it can be retried
+				return reconcile.Result{}, err
+			}
+
+			// remove our finalizer from the list and update it.
+			zeebeCluster.ObjectMeta.Finalizers = removeString(zeebeCluster.ObjectMeta.Finalizers, myFinalizerName)
+			if err := r.Update(context.Background(), &zeebeCluster); err != nil {
+				return reconcile.Result{}, err
+			}
+		}
+		return reconcile.Result{}, nil
+	}
+
+	if zeebeCluster.Status.ClusterStatus.Ready == "" {
+		clusterId, err := cc.CreateCluster(req.NamespacedName.Name)
+		if err != nil {
+			log.Error(err, "failed to create cluster")
+			return reconcile.Result{}, err
+		}
+		log.Info("Updating Zeebe Cluster with: ", "ClusterId", clusterId)
+		zeebeCluster.Status.ClusterId = clusterId
+
+		if err := r.Update(context.Background(), &zeebeCluster); err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+	if zeebeCluster.Status.ClusterId != "" {
+		clusterStatus, err := r.WaitForClusterStateChange(zeebeCluster.Status.ClusterId, zeebeCluster.Status.ClusterStatus)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		zeebeCluster.Status.ClusterStatus = clusterStatus
+		if err := r.Update(context.Background(), &zeebeCluster); err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *ZeebeClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&zeebev1.ZeebeCluster{}).
+		Complete(r)
+}
+
+func (r *ZeebeClusterReconciler) deleteExternalDependency(zeebeCluster *zeebev1.ZeebeCluster) error {
+	log.Printf("Trying to delete the cluster in camunda cloud")
+
+	deleted, err := cc.DeleteCluster(zeebeCluster.Status.ClusterId)
+	if err != nil {
+		log.Fatal(err, "Failed to delete cluster")
+	}
+	if deleted {
+		log.Printf("Cluster in camunda cloud deleted")
+	}
+
+	return nil
+
+}
+
+//
+// Helper functions to check and remove string from a slice of strings.
+//
+func containsString(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func removeString(slice []string, s string) (result []string) {
+	for _, item := range slice {
+		if item == s {
+			continue
+		}
+		result = append(result, item)
+	}
+	return
+}
---
+++ main.go
@@ -0,0 +1,126 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	cc "github.com/salaboy/camunda-cloud-go-client/pkg/cc/client"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
+
+	"context"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	zeebev1 "zeebe.io/m/v2/api/v1"
+	"zeebe.io/m/v2/controllers"
+	// +kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	_ = clientgoscheme.AddToScheme(scheme)
+
+	_ = zeebev1.AddToScheme(scheme)
+	// +kubebuilder:scaffold:scheme
+}
+
+var clientId = os.Getenv("CC_CLIENT_ID")
+var clientSecret = os.Getenv("CC_CLIENT_SECRET")
+
+func main() {
+	var metricsAddr string
+	var enableLeaderElection bool
+	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	flag.Parse()
+
+	ctrl.SetLogger(zap.Logger(true))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:             scheme,
+		MetricsBindAddress: metricsAddr,
+		LeaderElection:     enableLeaderElection,
+		Port:               9443,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.ZeebeClusterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("ZeebeCluster"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ZeebeCluster")
+		os.Exit(1)
+	}
+	// +kubebuilder:scaffold:builder
+	setupLog.Info("Attempting to login to Camunda Cloud ...")
+	var loginOk, errCC = cc.Login(clientId, clientSecret)
+	if errCC != nil {
+		setupLog.Error(errCC, "Cannot connect to Camunda Cloud. Check CC_CLIENT_ID and CC_CLIENT_SECRET")
+		os.Exit(1)
+	}
+	if loginOk {
+		setupLog.Info("Logged in!")
+		//Getting available plans on startup.. then working with those for all requests
+		setupLog.Info("Retrieving Cluster Plans ...")
+		cc.GetClusterParams()
+		setupLog.Info("Retrieving Cluster Plans Done! ")
+		clusters, err := cc.GetClusters()
+		if err != nil {
+			setupLog.Error(err, "failed to get clusters")
+		}
+		setupLog.Info("reported clusters", "Clusters: ", clusters)
+		for _, c := range clusters {
+			ctx := context.Background()
+			var zeebeCluster = zeebev1.ZeebeCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      c.Name,
+					Namespace: "default",
+				},
+				Status: zeebev1.ZeebeClusterStatus{
+					ClusterId: c.ID,
+					ClusterStatus: cc.ClusterStatus{
+						Ready: "Fetching",
+					},
+				},
+			}
+			err := mgr.GetClient().Create(ctx, &zeebeCluster)
+			if err != nil {
+				setupLog.Error(err, "Error Creating ZeebeCluster: "+c.Name)
+			}
+		}
+
+	} else {
+		setupLog.Info("Error login in!")
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+
+}
